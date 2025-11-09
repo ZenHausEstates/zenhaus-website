a4f2071ae557d569dd25a50530585305b327a0f0
ZenHausEstates <mirko@zenhausestates.com>
2025-11-09 08:28:43 -0500

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{padding: 24}}>
-      <h1>ZenHaus Estates</h1>
-      <p>Based in Venice, FL — Serving Southwest Florida (Tampa Bay to Naples).</p>
-      <p><a href="/about">About</a></p>
-    </div>
-  );
-}
